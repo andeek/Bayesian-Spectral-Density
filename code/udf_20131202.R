92808a2d07b695f54ccce1a6ab91cd74fc847925
andeek <ajkaplan@iastate.edu>
2013-12-02 20:27:56 -0600

    wrote a couple helper functions

---
+++ code/udf_20131202.R
@@ -0,0 +1,20 @@
+##Helper functions for simulation/estimation of necessary spacing
+##12/2/2013
+
+periodogram <- function(y, freq, center=TRUE, plot=TRUE) {
+  require(ggplot2)
+  
+  if(center) y <- y - mean(y)
+  
+  n <- length(y)
+  spec <- sapply(freq, function(x) 1/n*((sum(y*cos(x*(1:n))))^2 + (sum(y*sin(x*(1:n))))^2)) 
+  df <- data.frame(freq = freq, spec= spec)
+  
+  if(plot) print(ggplot(df, aes(x=freq, y=spec)) + geom_line() + geom_point(size=2, shape=1) + xlab("Frequency") + ylab("") + ggtitle("Periodogram"))
+  
+  return(df)
+}
+
+fourier_freq <- function(n) {
+  2*pi/n*(1:floor(n/2))
+}
